6d37cdfdc49f48e8c967b2423695513ce3b65861
deji <dosagie@beyondtrust.com>
2023-12-28 14:51:51 -0400

    feat: Adding Bank account simulation

--- script.py
+++ script.py
@@ -1,38 +1,48 @@
-# Testing AceBank Class in src/BankAccount
+# Testing AceBank Class in src/AceBank
+import logging
+import os
+import sys
 
 from src.BankAccount import AceBank
 
-if __name__ == '__main__':
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S %p",
+)
+LOG = logging.getLogger(os.path.basename(sys.argv[0]))
+
+if __name__ == "__main__":
     # initiate AceBank with customer id 234
-    ab = AceBank()
+    ab = AceBank(LOG)
     # setup account for customer 234
-    ab.create_account(account_number='0808', balance=1000.00, customer_id=234)
+    ab.create_account(account_number="0808", balance=1000.00, customer_id=234)
     # deposit money for account 0808
-    ab.deposit_fund('0808', 500.00, 'usdollars', 234)
+    ab.deposit_fund("0808", 500.00, "USD", 234)
 
     # withdraw from account 0808
-    ab.withdraw_funds('0808', 100.00, 'caddollars', 234)
+    ab.withdraw_funds("0808", 100.00, "CAD", 234)
 
     # get balance from account 0808
-    balance = ab.get_balance('0808', 234)
-    print(f"Balance of account 0808 after last transaction is {balance}")
+    balance = ab.get_balance("0808", 234)
+    LOG.info(f"Balance of account 0808 after last transaction is {balance}")
 
     # initiate AceBank with customer id 756
-    ab = AceBank()
+    ab = AceBank(LOG)
     # setup accounts for customer 756
-    ab.create_account(account_number='0903', balance=100.00, customer_id=756)
-    ab.create_account(account_number='0875', balance=6000.00, customer_id=756)
+    ab.create_account(account_number="0903", balance=100.00, customer_id=756)
+    ab.create_account(account_number="0875", balance=6000.00, customer_id=756)
 
     # withdraw from account 0875
-    ab.withdraw_funds('0875', 700.00, 'usdollars', 756)
+    ab.withdraw_funds("0875", 700.00, "USD", 756)
     # deposit fund to 0903
-    ab.deposit_fund('0903', 2500.00, 'euros', 756)
+    ab.deposit_fund("0903", 2500.00, "EUROS", 756)
 
     # transfer fund from 0875 to 0903
-    ab.transfer_funds('0875', '0903', 1100.00, 756, 756)
+    ab.transfer_funds("0875", "0903", 1100.00, 756, 756)
 
     # get balance from account 0808
-    balance_0875 = ab.get_balance('0875', 756)
-    balance_0903 = ab.get_balance('0903', 756)
-    print(f"Balance of account 0875 after last transaction is {balance_0875}")
-    print(f"Balance of account 0903 after last transaction is {balance_0903}")
+    balance_0875 = ab.get_balance("0875", 756)
+    balance_0903 = ab.get_balance("0903", 756)
+    LOG.info(f"Balance of account 0875 after last transaction is {balance_0875}")
+    LOG.info(f"Balance of account 0903 after last transaction is {balance_0903}")
--- tests/unit_test/test_bank_account.py
+++ tests/unit_test/test_bank_account.py
@@ -1,161 +1,163 @@
 """
 Unit test for AceBank class and all the functions in it
 """
+import logging
+
 import pytest
 from src.BankAccount import AceBank
 
-ACCEPTED_CURRENCY = ['usdollars', 'euros', 'caddollars']
+logging.basicConfig(
+    filename="bank_app.logs",
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S %p",
+)
 
-
-@pytest.fixture
-def ace_bank():
-    return AceBank()
+ACCEPTED_CURRENCY = ["USD", "EUROS", "CAD"]
 
 
 @pytest.fixture
 def mock_bank():
     account_details = {
-        250: {
-            '8012': {
-                'balance': 1000.00
-            },
-            '7095': {
-                'balance': 32500.95
-            }
-        },
-        650: {
-            '4000': {
-                'balance': 350.10
-            }
-        }
+        250: {"8012": {"balance": 1000.00}, "7095": {"balance": 32500.95}},
+        650: {"4000": {"balance": 350.10}},
     }
 
-    ab = AceBank()
-    ab.account_details = account_details
+    ab = AceBank(logging, account_details=account_details)
     return ab
 
 
-class TestAceBank(object):
+@pytest.fixture
+def ace_bank():
+    return AceBank(logging)
 
+
+class TestAceBank(object):
     def test_create_account(self, ace_bank):
         """
         This test creates an account function
         """
-        ace_bank.create_account('0912', '10000', 50)
-        ace_bank.validate_account('0912', 50)
-        assert ace_bank.account_details == {50: {"0912": {'balance': 10000}}}
-        assert ace_bank.account_details[50] == {"0912": {'balance': 10000}}
-        assert ace_bank.account_details[50]['0912'] == {'balance': 10000}
+        ace_bank.create_account("0912", "10000", 50)
+        ace_bank.validate_account("0912", 50)
+        assert ace_bank.account_details == {50: {"0912": {"balance": 10000}}}
+        assert ace_bank.account_details[50] == {"0912": {"balance": 10000}}
+        assert ace_bank.account_details[50]["0912"] == {"balance": 10000}
 
     def test_get_balance(self, ace_bank):
         # test no account
-        with pytest.raises(ValueError) as noaccount:
-            ace_bank.get_balance('1010', 50)
-        assert "Account does not exist" in str(noaccount.value)
+        with pytest.raises(ValueError) as no_account:
+            ace_bank.get_balance("1010", 50)
+        assert "Account does not exist" in str(no_account.value)
 
         # do proper testing to get balance
-        ace_bank.create_account('08912', '10000.0', 50)
-        acct_balance = ace_bank.get_balance('08912', 50)
+        ace_bank.create_account("08912", "10000.0", 50)
+        acct_balance = ace_bank.get_balance("08912", 50)
         assert acct_balance == 10000.0
 
     def test_deposit_fund(self, mock_bank):
         # test deposit fund by using us dollars
-        assert mock_bank.account_details[250]['8012']['balance'] == 1000.00
-        mock_bank.deposit_fund('8012', 200.00, 'usdollars', 250)
-        assert mock_bank.account_details[250]['8012']['balance'] == 1300.00
+        assert mock_bank.account_details[250]["8012"]["balance"] == 1000.00
+        mock_bank.deposit_fund("8012", 200.00, "USD", 250)
+        assert mock_bank.account_details[250]["8012"]["balance"] == 1300.00
 
-        # test deposit fund using euros
-        assert mock_bank.account_details[250]['8012']['balance'] == 1300.00
-        mock_bank.deposit_fund('8012', 200.00, 'euros', 250)
-        assert mock_bank.account_details[250]['8012']['balance'] == 1700.00
+        # test deposit fund using EUROS
+        assert mock_bank.account_details[250]["8012"]["balance"] == 1300.00
+        mock_bank.deposit_fund("8012", 200.00, "EUROS", 250)
+        assert mock_bank.account_details[250]["8012"]["balance"] == 1700.00
 
         # test deposit fund using cad
-        assert mock_bank.account_details[250]['8012']['balance'] == 1700.00
-        mock_bank.deposit_fund('8012', 200.00, 'caddollars', 250)
-        assert mock_bank.account_details[250]['8012']['balance'] == 1900.00
+        assert mock_bank.account_details[250]["8012"]["balance"] == 1700.00
+        mock_bank.deposit_fund("8012", 200.00, "CAD", 250)
+        assert mock_bank.account_details[250]["8012"]["balance"] == 1900.00
 
     def test_withdraw_funds(self, mock_bank):
-        # test deposit fund using euros
-        assert mock_bank.account_details[250]['8012']['balance'] == 1000.00
-        mock_bank.withdraw_funds('8012', 200.00, 'euros', 250)
-        assert mock_bank.account_details[250]['8012']['balance'] == 600.00
+        # test deposit fund using EUROS
+        assert mock_bank.account_details[250]["8012"]["balance"] == 1000.00
+        mock_bank.withdraw_funds("8012", 200.00, "EUROS", 250)
+        assert mock_bank.account_details[250]["8012"]["balance"] == 600.00
 
         # test deposit fund by using us dollars
-        assert mock_bank.account_details[650]['4000']['balance'] == 350.10
-        mock_bank.withdraw_funds('4000', 200.00, 'usdollars', 650)
-        assert mock_bank.account_details[650]['4000']['balance'] == 50.10
+        assert mock_bank.account_details[650]["4000"]["balance"] == 350.10
+        mock_bank.withdraw_funds("4000", 200.00, "USD", 650)
+        assert mock_bank.account_details[650]["4000"]["balance"] == 50.10
 
         # test deposit fund by using us dollars
-        assert mock_bank.account_details[250]['7095']['balance'] == 32500.95
-        mock_bank.withdraw_funds('7095', 2593.75, 'caddollars', 250)
-        assert mock_bank.account_details[250]['7095']['balance'] == 29907.20
+        assert mock_bank.account_details[250]["7095"]["balance"] == 32500.95
+        mock_bank.withdraw_funds("7095", 2593.75, "CAD", 250)
+        assert mock_bank.account_details[250]["7095"]["balance"] == 29907.20
 
     def test_transfer_fund(self, mock_bank):
         # confirm how much was in each account before transfer
-        assert mock_bank.account_details[250]['7095']['balance'] == 32500.95
-        assert mock_bank.account_details[250]['8012']['balance'] == 1000.00
+        assert mock_bank.account_details[250]["7095"]["balance"] == 32500.95
+        assert mock_bank.account_details[250]["8012"]["balance"] == 1000.00
         # make transfer
-        mock_bank.transfer_funds('8012', '7095', 2000.00, 250, 250)
+        mock_bank.transfer_funds("8012", "7095", 2000.00, 250, 250)
         # confirm changes
-        assert mock_bank.account_details[250]['7095']['balance'] == 30500.95
-        assert mock_bank.account_details[250]['8012']['balance'] == 3000.00
+        assert mock_bank.account_details[250]["7095"]["balance"] == 30500.95
+        assert mock_bank.account_details[250]["8012"]["balance"] == 3000.00
 
     def test_validate_account(self, mock_bank):
-        # test account that dosnt exist
+        # test account that does not exist
         with pytest.raises(ValueError) as va:
-            mock_bank.validate_account('1010', 250)
+            mock_bank.validate_account("1010", 250)
         assert "Account does not exist" in str(va.value)
 
         # test account that exist
-        mock_bank.validate_account('4000', 650)
+        mock_bank.validate_account("4000", 650)
 
     def test_validate_balance(self):
         # test when account_balance is greater than amount
-        AceBank().validate_balance(1000, 400)
+        AceBank(logging).validate_balance(1000, 400)
 
         # test when account_balance is equal amount
-        AceBank().validate_balance(400, 400)
+        AceBank(logging).validate_balance(400, 400)
 
         # test when account_balance is less than amount
-        with pytest.raises(ValueError) as lowbalance:
-            AceBank().validate_balance(300, 400)
-        assert "Account balance of 300 is currently lesser than 400" in \
-               str(lowbalance.value)
+        with pytest.raises(ValueError) as low_balance:
+            AceBank(logging).validate_balance(300, 400)
+        assert "Account balance of 300 is currently lesser than 400" in (
+            str(low_balance.value)
+        )
 
-    @pytest.mark.parametrize('account_number, amount, currency, '
-                             'outgoing_acct_num',
-                             [(200.00, 200, 1, 854.2),
-                              (200, 'error', 'cad', 2)])
-    def test_validate_variable(self, account_number, amount, currency,
-                               outgoing_acct_num):
-        ac = AceBank()
+    @pytest.mark.parametrize(
+        "account_number, amount, currency, outgoing_acct_num",
+        [(200.00, 200, 1, 854.2), (200, "error", "cad", 2)],
+    )
+    def test_validate_variable(
+        self, account_number, amount, currency, outgoing_acct_num
+    ):
+        ac = AceBank(logging)
         # test when account_number
-        with pytest.raises(ValueError) as wrongvariable:
+        with pytest.raises(ValueError) as wrong_variable:
             ac.validate_variable(account_number=account_number)
-        assert f"Account number {account_number} must be string" \
-               in str(wrongvariable.value)
+        assert f"Account number {account_number} must be string" in str(
+            wrong_variable.value
+        )
 
         # test when amount
-        with pytest.raises(ValueError) as wrongvariable:
+        with pytest.raises(ValueError) as wrong_variable:
             ac.validate_variable(amount=amount)
-        assert f"Amount must be of type float" in str(wrongvariable.value)
+        assert "Amount must be of type float" in str(wrong_variable.value)
 
         # test when currency
-        with pytest.raises(ValueError) as wrongvariable:
+        with pytest.raises(ValueError) as wrong_variable:
             ac.validate_variable(currency=currency)
-        assert f"Currency {currency} is not recognized. Should be one of " \
-               f"{ACCEPTED_CURRENCY}" in str(wrongvariable.value)
+        assert (
+            f"Currency {currency} is not recognized. Should be one of "
+            f"{ACCEPTED_CURRENCY}" in str(wrong_variable.value)
+        )
 
         # test when outgoing_account_number
-        with pytest.raises(ValueError) as wrongvariable:
+        with pytest.raises(ValueError) as wrong_variable:
             ac.validate_variable(outgoing_account_number=outgoing_acct_num)
-        assert f"Account number {outgoing_acct_num} must be string" \
-               in str(wrongvariable.value)
+        assert f"Account number {outgoing_acct_num} must be string" in str(
+            wrong_variable.value
+        )
 
     def test_currency_converter(self):
-        ac = AceBank()
+        ac = AceBank(logging)
         # test converting us dollars to cad
-        assert ac.currency_converter(100, 'usdollars') == 150
+        assert ac.currency_converter(100, "USD") == 150
 
-        # test converting euros to cad
-        assert ac.currency_converter(100, 'euros') == 200
+        # test converting EUROS to cad
+        assert ac.currency_converter(100, "EUROS") == 200
